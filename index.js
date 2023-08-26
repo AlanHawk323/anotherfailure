c7b6dc3e7a0126c92c9aad515be0f962e0c91b9d
AlanHawk323 <alanhawk323@gmail.com>
2023-08-26 11:23:47 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,81 +1,54 @@
-console.log("testing testing")
+// // TODO: this file! :)
+const numList = document.querySelector("form");
+const nums = {numbers: [], odds: [], evens: []};
+const numBank = document.querySelector("#numberBank output");
 
-//const submitButton = document.querySelector("button");
-// console.log(submitButton)
-
-const digits =
-{
-    numbers: [],
-    odds: [],
-    evens: []
-}
-
-const form = document.querySelector("form")
-const numberBank = document.querySelector("#numberBank output");
-const oddBank = document.querySelector("#odds output");
-const evenBank = document.querySelector("#evens output");
-
-function render ()
-{const numbers = digits.number.map((num) => 
+function render()
+{const outputNums = nums.numbers.map((num) =>
     {return `<span> ${num} </span>`
 })
+numBank.innerHTML = outputNums.join("")
 }
 
-form.addEventListener("submit", (event) => {event.preventDefault()
-const addedNum = event.target.number.value
-digits.number.push(addedNum)
-render()
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const numAdded = event.target.number.value
+    nums.numbers.push(numAdded)
+    render()
 })
 
-// function render()
-//     {const startNumbers = digits.number.map((numbers) =>
-//         {return `<span> ${numbers} </span>`})
-//         numberBank.innerHTML = startNumbers.join("")
-//     }
+const oddFilter = document.querySelector("#oddFilter output");
+const evenFilter = document.querySelector("#evenFilter output");
 
-//         form.addEventListener("submit", (event) => {event.preventDefault()
-//         const newNum = event.target.startNumbers.value
+function render()
+{const oddNums = nums.odds.map((num) =>
+    {return `<span> ${num} </span>`
+    })
+    oddFilter.innerHTML = oddNums.join("")
+}
 
-//         digits.number.push(newNum)
-//         render()
-//         })
+function render()
+{const evenNums = nums.evens.map((num) =>
+    {return `<span> ${num} </span>`
+    })
+    evenFilter.innerHTML = evenNums.join("")
+}
 
+const sortOne = document.querySelector("#sortOne");
+const sortAll = document.querySelector("#sortAll");
 
-        //Everything below is the remnants of failed attempts. I struggled a lot with this.
-    //     const form = Array.from(document.getElementById("form"))
-    //     console.log(form)
-    //     form.map((numbers) =>
-    //     {numbers.addEventListener("submit", () => {digits.target.push(digits.numbers.pop())
-    //     render()
-    //     })
-    // })
-    // const boxNumbers = digits.target.map((numbers) =>
-    //     {return `<output> ${numbers} </output>`})
-    //     finalNums.innerHTML = boxNumbers.join("")
-    //     }
-    //     render()
+sortOne.addEventListener("click", (num) =>
+{nums.number.find()
+    if(toSort % 2 === 0)
+    {nums.evens.push(num)}
+    else{nums.odds.push(num)}
+    render()
+})
 
-
-// function render()
-// {const allNums = bank.numbers.map((num) =>
-//     {return `<span> ${num} </span>`})
-//     userNumbers.innerHTML = allNums.join("");
-// }
-
-// userNumbers.addEventListener("submit", (event) =>
-//     {event.preventDefault()
-//         const added = event.target.userNumbers.value
-//         bank.userNumbers.push(added)
-//         render()
-// })
-
-    // const submitButton = document.querySelector("button");
-    // submitButton.addEventListener("click", (event) =>
-    // {event.preventDefault()
-    //     console.log(event.target.number.value)
-    //     numbers.push(event.target.number.value)
-    //     console.log("Your numbers:", numbers)
-    //     event.target.number.value = ""
-    //     render()
-    // })
-//I was at this for actual hours. I don't understand why it's not working. I'm so tired of my functions being undefined.
+sortAll.addEventListener("click", (num) =>
+    {nums.number.map()
+    if(num % 2 === 0)
+    {nums.evens.push(num)}
+    else{nums.odds.push(num)}
+    render()
+    })
